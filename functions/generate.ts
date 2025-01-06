f20d4a547c85cc07f29359e1c22157c51c718592
Leo Liu <11664880+silver886@users.noreply.github.com>
2025-01-06 03:03:24 -0800

    implement download

--- functions/generate.ts
+++ functions/generate.ts
@@ -2,7 +2,12 @@ import {generateCalendar} from '../lib/calendar';
 import {parseTxt} from '../lib/txt';
 
 export const onRequestPost: PagesFunction = async (context) => {
-   return new Response(
-      generateCalendar(parseTxt(await context.request.text())).toString(),
-   );
+   const schedule = parseTxt(await context.request.text());
+
+   return new Response(generateCalendar(schedule).toString(), {
+      headers: {
+         'Content-Type': 'text/calendar; charset=utf-8',
+         'Content-Disposition': `attachment; filename*=UTF-8''${encodeURIComponent(`${schedule.aNumber}-${schedule.term}-${schedule.createdTime}.ics`)}`,
+      },
+   });
 };
